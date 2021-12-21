07efd2af51a5ca5b54a41d08c0352f096d3708ea
Siddhant Kumar <siddhantkumar.xyz@gmail.com>
2021-12-21 23:17:54 +0530

    Changes done

--- lib/Test.rb
+++ lib/Test.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
-
+require "open-uri"
+require "nokogiri"
 require_relative "Test/version"
 
 module Test
   class Error < StandardError; end
   # Your code goes here...
-  puts "Hello World"
+  #puts "Hello World"
+  #URI.open("https://docs.cloudera.com") do |f|
+   # puts f.read
+ # end
+  doc = Nokogiri::HTML(open("https://docs.cloudera.com"))
+  #p doc
+  title = doc.xpath('//title').text
+  puts title
 end
+
+
